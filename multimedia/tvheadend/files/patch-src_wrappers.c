f9bb19c2e0e01258596f294f614cc8ed97664f48
Bernhard Fröhlich <decke@bluelife.at>
2017-02-08 12:21:04 +0100

    - Add patch to fix build for systems with do not have clock_nanosleep() - Disable PIE because it currently does not build properly. Needs more investigation.

--- multimedia/tvheadend/files/patch-src_wrappers.c
+++ multimedia/tvheadend/files/patch-src_wrappers.c
@@ -1,5 +1,5 @@
 --- src/wrappers.c.orig	2017-01-20 19:41:51.000000000 +0100
-+++ src/wrappers.c	2017-02-08 11:35:24.814465000 +0100
++++ src/wrappers.c	2017-02-08 12:18:01.540169000 +0100
 @@ -191,6 +191,8 @@
    pid_t tid;
    tid = gettid();
@@ -9,3 +9,39 @@
  #else
  #warning "Implement renice for your platform!"
  #endif
+@@ -298,9 +300,16 @@
+   int r;
+   if (us <= 0)
+     return 0;
++
+   ts.tv_sec = us / 1000000LL;
+   ts.tv_nsec = (us % 1000000LL) * 1000LL;
++
++#ifdef clock_nanosleep
+   r = clock_nanosleep(CLOCK_MONOTONIC, 0, &ts, &ts);
++#else
++  r = nanosleep(&ts, NULL);
++#endif
++
+   val = (ts.tv_sec * 1000000LL) + ((ts.tv_nsec + 500LL) / 1000LL);
+   if (ERRNO_AGAIN(r))
+     return val;
+@@ -315,9 +324,18 @@
+   int r;
+   if (us <= 0)
+     return 0;
++
++#ifdef clock_nanosleep
+   ts.tv_sec = us / 1000000LL;
+   ts.tv_nsec = (us % 1000000LL) * 1000LL;
+   r = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, &ts);
++#else
++  us -= getmonoclock();
++
++  ts.tv_sec = us / 1000000LL;
++  ts.tv_nsec = (us % 1000000LL) * 1000LL;
++  r = nanosleep(&ts, NULL);
++#endif
+   val = (ts.tv_sec * 1000000LL) + ((ts.tv_nsec + 500LL) / 1000LL);
+   if (ERRNO_AGAIN(r))
+     return val;
