1183d4c7af8b4fd1c28b9754dfa024b75d9a969d
Bernhard Fröhlich <decke@bluelife.at>
2017-06-21 11:26:48 +0200

    Add new implementation based on usleep() to avoid clock_nanosleep() usage. Starting with FreeBSD 11.1 we have a proper clock_nanosleep() but we still need to support 10.x and 11.0 until Q12018.

--- multimedia/tvheadend/files/patch-src_wrappers.c
+++ multimedia/tvheadend/files/patch-src_wrappers.c
@@ -1,5 +1,5 @@
---- src/wrappers.c.orig	2017-04-21 08:32:22.000000000 +0000
-+++ src/wrappers.c	2017-06-20 14:13:41.940132000 +0000
+--- src/wrappers.c.orig	2017-05-16 11:15:24.000000000 +0000
++++ src/wrappers.c	2017-06-21 11:23:54.840229000 +0000
 @@ -191,6 +191,8 @@
    pid_t tid;
    tid = gettid();
@@ -9,3 +9,31 @@
  #else
  #warning "Implement renice for your platform!"
  #endif
+@@ -290,6 +292,19 @@
+   } while (r > 0);
+ }
+ 
++#ifdef PLATFORM_FREEBSD
++int64_t
++tvh_usleep(int64_t us)
++{
++  return usleep(us);
++}
++
++int64_t
++tvh_usleep_abs(int64_t us)
++{
++  return usleep(us - getfastmonoclock());
++}
++#else
+ int64_t
+ tvh_usleep(int64_t us)
+ {
+@@ -323,6 +338,7 @@
+     return val;
+   return r ? -r : 0;
+ }
++#endif
+ 
+ /*
+  * qsort
