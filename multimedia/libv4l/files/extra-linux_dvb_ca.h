faa2e77f78bca50f08455f9e740a5966a3e82ee3
Bernhard Froehlich <decke@bluelife.at>
2018-07-12 15:43:59 +0200

    multimedia/libv4l: Update to 1.14.2 and update kernel headers to Kernel 4.17.2

--- multimedia/libv4l/files/extra-linux_dvb_ca.h
+++ multimedia/libv4l/files/extra-linux_dvb_ca.h
@@ -1,20 +1,18 @@
---- ../linux/dvb/ca.h.orig	2014-12-01 10:58:18.000000000 +0100
-+++ ../linux/dvb/ca.h	2015-01-23 22:41:21.761540460 +0100
-@@ -80,9 +80,17 @@
+--- ../linux/dvb/ca.h.orig	2018-06-20 14:17:16.975230000 +0200
++++ ../linux/dvb/ca.h	2018-06-20 14:22:58.803607000 +0200
+@@ -134,9 +134,13 @@
  
  #define CA_RESET          _IO('o', 128)
- #define CA_GET_CAP        _IOR('o', 129, ca_caps_t)
+ #define CA_GET_CAP        _IOR('o', 129, struct ca_caps)
+-#define CA_GET_SLOT_INFO  _IOR('o', 130, struct ca_slot_info)
 +
 +/* At least CA_GET_SLOT_INFO and CA_GET_MSG need to be _IOWR not _IOR.
 + * This is wrong on Linux too but there the driver doesn't care.
-+ *
- #define CA_GET_SLOT_INFO  _IOR('o', 130, ca_slot_info_t)
- #define CA_GET_DESCR_INFO _IOR('o', 131, ca_descr_info_t)
- #define CA_GET_MSG        _IOR('o', 132, ca_msg_t)
 + */
-+#define CA_GET_SLOT_INFO  _IOWR('o', 130, ca_slot_info_t)
-+#define CA_GET_DESCR_INFO _IOR('o', 131, ca_descr_info_t)
-+#define CA_GET_MSG        _IOWR('o', 132, ca_msg_t)
- #define CA_SEND_MSG       _IOW('o', 133, ca_msg_t)
- #define CA_SET_DESCR      _IOW('o', 134, ca_descr_t)
- #define CA_SET_PID        _IOW('o', 135, ca_pid_t)
++#define CA_GET_SLOT_INFO  _IOWR('o', 130, struct ca_slot_info)
+ #define CA_GET_DESCR_INFO _IOR('o', 131, struct ca_descr_info)
+-#define CA_GET_MSG        _IOR('o', 132, struct ca_msg)
++#define CA_GET_MSG        _IOWR('o', 132, struct ca_msg)
+ #define CA_SEND_MSG       _IOW('o', 133, struct ca_msg)
+ #define CA_SET_DESCR      _IOW('o', 134, struct ca_descr)
+ 
