7bee8fa37fc1a9e5c9de9f493170ce853aa986d3
Bernhard Froehlich <decke@bluelife.at>
2019-09-05 09:00:15 +0000

    sysutils/docker-engine: Update to 19.03.2

--- sysutils/docker-engine/files/patch-vendor_github.com_moby_buildkit_executor_oci_spec__unix.go
+++ sysutils/docker-engine/files/patch-vendor_github.com_moby_buildkit_executor_oci_spec__unix.go
@@ -1,4 +1,4 @@
---- vendor/github.com/moby/buildkit/executor/oci/spec_unix.go.orig	2019-06-18 21:30:11 UTC
+--- vendor/github.com/moby/buildkit/executor/oci/spec_unix.go.orig	2019-08-22 20:57:25 UTC
 +++ vendor/github.com/moby/buildkit/executor/oci/spec_unix.go
 @@ -8,7 +8,6 @@ import (
  	"sync"
@@ -8,7 +8,7 @@
  	"github.com/containerd/containerd/mount"
  	"github.com/containerd/containerd/namespaces"
  	"github.com/containerd/containerd/oci"
-@@ -52,7 +51,8 @@ func GenerateSpec(ctx context.Context, meta executor.M
+@@ -40,7 +39,8 @@ func GenerateSpec(ctx context.Context, meta executor.M
  	if meta.SecurityMode == pb.SecurityMode_INSECURE {
  		opts = append(opts, entitlements.WithInsecureSpec())
  	} else if system.SeccompSupported() && meta.SecurityMode == pb.SecurityMode_SANDBOX {
