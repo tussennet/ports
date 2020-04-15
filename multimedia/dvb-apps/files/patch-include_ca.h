cf7c9e56f7675c2ddeb534e4702f3ca189e2f689
Bernhard Froehlich <decke@bluelife.at>
2020-04-15 18:48:20 +0000

    multimedia/dvb-apps: Add proper headers to new patch files for better traceability

--- multimedia/dvb-apps/files/patch-include_ca.h
+++ multimedia/dvb-apps/files/patch-include_ca.h
@@ -1,3 +1,11 @@
+From ed58fb83ffaf70423a83866a738978cc273f877b Mon Sep 17 00:00:00 2001
+From: Chris Reffett <creffett@gentoo.org>
+Date: Thu, 4 Jan 2018 13:13:00 -0500
+Subject: media-tv/linuxtv-dvb-apps: Remove references to deleted CA_SET_PID
+ syscall
+
+Closes: https://bugs.gentoo.org/643436
+Package-Manager: Portage-2.3.19, Repoman-2.3.6
 --- include/ca.h
 +++ include/ca.h
 @@ -85,6 +85,5 @@
