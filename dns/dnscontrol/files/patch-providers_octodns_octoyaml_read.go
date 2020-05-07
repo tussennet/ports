11ee4239da29a861f9e4176160e077ac3ad5dbdb
Bernhard Froehlich <decke@bluelife.at>
2020-05-07 09:23:32 +0000

    dns/dnscontrol: Fix patch

--- dns/dnscontrol/files/patch-providers_octodns_octoyaml_read.go
+++ dns/dnscontrol/files/patch-providers_octodns_octoyaml_read.go
@@ -5,7 +5,15 @@ Obtained from:	https://github.com/StackExchange/dnscontrol/issues/736
 
 --- providers/octodns/octoyaml/read.go.orig	2020-05-03 15:43:54 UTC
 +++ providers/octodns/octoyaml/read.go
-@@ -263,7 +263,7 @@ func decodeTTL(ttl interface{}) (uint32, error) {
+@@ -12,7 +12,6 @@ import (
+ 	"fmt"
+ 	"io"
+ 	"io/ioutil"
+-	"math"
+ 	"reflect"
+ 	"strconv"
+ 
+@@ -263,7 +262,7 @@ func decodeTTL(ttl interface{}) (uint32, error) {
  		return uint32(t), fmt.Errorf("decodeTTL failed to parse (%s): %w", s, err)
  	case int:
  		i := ttl.(int)
