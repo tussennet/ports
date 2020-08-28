a125fbe914e950425c0f3c7172b6d4afee00132c
tussennet <test@test.nl>
2020-08-28 12:02:00 +0200

    removed unused property

--- sysutils/docker-engine/files/patch-daemon_container_operations_unix.go
+++ sysutils/docker-engine/files/patch-daemon_container_operations_unix.go
@@ -1,12 +1,14 @@
 --- daemon/container_operations_unix.go.orig	2020-08-28 10:10:01.856418000 +0200
 +++ daemon/container_operations_unix.go	2020-08-28 10:10:16.078675000 +0200
-@@ -144,9 +144,9 @@
+@@ -143,10 +143,10 @@
+ 				return err
  			}
  
- 			shmproperty := "mode=1777,size=" + strconv.FormatInt(c.HostConfig.ShmSize, 10)
+-			shmproperty := "mode=1777,size=" + strconv.FormatInt(c.HostConfig.ShmSize, 10)
 -			if err := unix.Mount("shm", shmPath, "tmpfs", uintptr(unix.MS_NOEXEC|unix.MS_NOSUID|unix.MS_NODEV), label.FormatMountLabel(shmproperty, c.GetMountLabel())); err != nil {
 -				return fmt.Errorf("mounting shm tmpfs: %s", err)
 -			}
++			// shmproperty := "mode=1777,size=" + strconv.FormatInt(c.HostConfig.ShmSize, 10)
 +			// if err := unix.Mount("shm", shmPath, "tmpfs", uintptr(unix.MS_NOEXEC|unix.MS_NOSUID|unix.MS_NODEV), label.FormatMountLabel(shmproperty, c.GetMountLabel())); err != nil {
 +			// 	return fmt.Errorf("mounting shm tmpfs: %s", err)
 +			// }
